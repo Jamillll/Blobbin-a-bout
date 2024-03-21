8f73def6ad6e6dc310fa1571ac58d6a2e6e0d7b2
Jamillll <mjmister2006@outlook.com>
2024-03-21 23:07:01 +0000

    Asset path management works?
    Can't test it right now and it feels kind of hacky but it should work

--- Blobbin-a-bout/source/Rendering/Texture2D.cpp
+++ Blobbin-a-bout/source/Rendering/Texture2D.cpp
@@ -3,7 +3,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-std::string Texture::m_BasePath = "C:\\Programs\\Blobbin-a-bout\\Blobbin-a-bout\\Assets\\";
+std::string Texture::m_BasePath = ".\\Assets\\";
 
 Texture::Texture(std::string texturePath)
 {
